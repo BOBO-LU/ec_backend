24e00dbb25482a1d8230e3fe2c34cec6bd213ebb
BOBO-LU <44457138+BOBO-LU@users.noreply.github.com>
2020-03-25 21:38:00 +0800

    Update development_config.js

--- config/development_config.js
+++ config/development_config.js
@@ -14,14 +14,3 @@ module.exports = {
     }
 }
 
-/*
-HOST = 'localhost'
-DATABASE_USER = 'test'
-DATABASE_PASSWORD = '1234'
-DATABASE = 'shopping_cart'
-
-SUPPRESS_NO_CONFIG_WARNING = 'disable'
-
-SEND_MAIL_USER = 'jonathan200415@gmail.com'
-SEND_MAIL_PASSWORD = 'jo0917222169' 
- */
